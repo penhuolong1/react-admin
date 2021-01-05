90ab32247cee08ef7d8261d888c82a81c00a6ef6
zengweikun <zengweikun@zonghenggroup.com.cn>
2021-01-05 16:38:58 +0800

    1

---
+++ src/router/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
+class Router extends Component {
+  render() {
+    return (
+      <div>
+        123123
+      </div>
+    );
+  }
+}
+
+export default Router;
