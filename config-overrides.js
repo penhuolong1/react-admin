956e000c08206f3a390b25057f7ce14b9d9f28f2
zengweikun <zengweikun@zonghenggroup.com.cn>
2021-01-06 17:29:35 +0800

    登陆页面样式

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,26 @@
 const path = require('path')
+const { override, adjustStyleLoaders, addWebpackAlias }  = require("customize-cra");
 
 function resolve (dir) {
   return path.join(__dirname, '.', dir)
 }
 
 /* config-overrides.js */
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  // alias
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    '@': resolve('src'),
-  };
-  return config;
-}
+module.exports = override(
+  // ...其他配置...
+  adjustStyleLoaders(rule => {
+    if (rule.test.toString().includes('scss')) {
+      rule.use.push({
+        loader: require.resolve('sass-resources-loader'),
+        options: {
+          resources: './src/styles/variables.scss'
+        }
+      });
+    }
+  }),
+  // 配置路径别名
+  addWebpackAlias({
+    "@": resolve("src"),
+  })
+  // ...其他配置...
+)
--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import './index.scss'
 
 class Login extends Component {
   render() {
     return (
-      <div class="login-wrapper">
-        <Form layout="inline">
-          <Form.Item label=":">
-            <Input onChange={this.filterTitleChange} />
-          </Form.Item>
-        </Form>
+      <div className="login-wrapper">
+        <div className="content">
+          <h2>用户登陆</h2>
+          <Form layout="inline">
+            <Form.Item
+              rules={[{ required: true, message: '请输入用户名!' }]}
+            >
+              <Input
+                placeholder="用户名"
+                name="username"
+                prefix={<UserOutlined className="form-icon" />}
+              />
+            </Form.Item>
+            <Form.Item
+              rules={[{ required: true, message: '请输入密码!' }]}
+            >
+              <Input.Password
+                placeholder="密码"
+                prefix={<LockOutlined  className="form-icon" />}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button className="login-btn" type="primary" htmlType="submit">
+                登陆
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     );
   }
