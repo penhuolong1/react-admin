d8a72e12f4ce62b9a45c5eb65c8ff25b2325b166
zengweikun <zengweikun@zonghenggroup.com.cn>
2021-01-06 16:13:47 +0800

    修改部分配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -507,7 +507,15 @@ module.exports = function (webpackEnv) {
                     : isEnvDevelopment,
                 },
                 'sass-loader'
-              ),
+              ).concat({
+                loader: 'sass-resources-loader',
+                options: {
+                  resources: [
+                    // 这里按照你的文件路径填写
+                    path.resolve(__dirname, '../src/styles/variables.scss')
+                  ]
+                }
+              }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
